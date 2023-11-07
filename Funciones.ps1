9bed4e8731fff592d7496cbc62ff7f824a2eed5b
oscarlim123 <oscarlim@protonmail.com>
2023-11-07 08:59:17 -0500

    Se agregaron comentarios

--- Funciones.ps1
+++ Funciones.ps1
@@ -1,25 +1,48 @@
 
 <#---------------------------------------------------------------------
+.DESCRIPTION
     Recibe una lista y crea un fichero con su contenido.
     Si ya existe se sobreescribe
+.PARAMETER list
+    Arreglo de strings que se guardará en el fichero resultante
+
+.PARAMETER rutaArchivo
+    Nombre del fichero que se quiere crear
+
 ---------------------------------------------------------------------#>
-function GuardarEnArchivo([string[]]$listado, [string]$rutaArchivo = "Resultados.txt") {    
-    $listado | Set-Content -Path $rutaArchivo -Force
+function GuardarEnArchivo([string[]]$list, [string]$rutaArchivo = "Resultados.txt") {    
+    $list | Set-Content -Path $rutaArchivo -Force
     Write-Host "Valores guardados en el archivo: $rutaArchivo"
 }
 
+
 <#---------------------------------------------------------------------
-
+.DESCRIPTION 
+    Recibe 2 direcciones IP y las valida
+.PARAMETER IPInicial
+    IP inicial
+.PARAMETER IPFinal
+    IP final
 ---------------------------------------------------------------------#>
-function FunctionName () {
-    $zonasHorarias = [System.TimeZoneInfo]::GetSystemTimeZones()
-    $zonaEncontrada = $false
+function CheckIP {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidatePattern("^(\d{1,3}\.){3}\d{1,3}$")]
+        [string]$IPInicial,
 
-    foreach ($zona in $zonasHorarias) {
-        if ($zona.Id -eq "Cuba Standard Time") {
-            $zonaEncontrada = $true
+        [Parameter(Mandatory=$true)]
+        [ValidatePattern("^(\d{1,3}\.){3}\d{1,3}$")]
+        [string]$IPFinal
+    )
+
+    try {
+        if ($inicio.Address -lt $fin.Address) {
             return $true
-            break
+        } else {
+            return $false
         }
+    } catch {
+        return $false
     }
 }
+
