d287da3b0ac87016ca432b3be4aa8804cd4eade1
oscarlim123 <oscarlim@protonmail.com>
2023-11-10 09:23:19 -0500

    Uso de Get-WmiObject

--- Get-TimeAD.ps1
+++ Get-TimeAD.ps1
@@ -1,36 +1,31 @@
+
+# Forzar la sincronización con el servidor de dominio
+#Write-Host "Forzando sincronización de tiempo en $computer con el servidor de dominio..."
+#Invoke-Command -ComputerName $computer -ScriptBlock { w32tm /resync /rediscover } -ErrorAction SilentlyContinue
+
 # Lista de nombres de computadoras del dominio
-$computadoras = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+$computerADList = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
 
-# Ruta del archivo de salida
-$archivoSalida = "ResultadoHoraComputadoras.txt"
+$archivoSalida = "HoraComputadorasAD.txt"
 
-# Iterar a través de las computadoras y obtener la hora
-foreach ($computadora in $computadoras) {
-    $online = Test-Connection -ComputerName $computadora -Count 2 -Quiet -ErrorAction SilentlyContinue
+foreach ($computer in $computerADList) {
+    $online = Test-Connection -ComputerName $computer -Count 2 -Quiet -ErrorAction SilentlyContinue
     
     if ($online) {
         try {
-            $session = New-PSSession -ComputerName $computadora -ErrorAction SilentlyContinue
+            #$hora = Invoke-Command -ComputerName $computer -ScriptBlock { Get-Date } -ErrorAction Stop
+            $objectHora = Get-WmiObject -Class Win32_LocalTime -ComputerName $computer -ErrorAction Stop
 
-            if($session.State -eq 'Opened'){
-                $hora = Invoke-Command -ComputerName $computadora -ScriptBlock { Get-Date } -ErrorAction Stop
-                $resultado = "Hora en $computadora : $hora"
-                Write-Host $resultado
-                Add-Content -Path $archivoSalida -Value $resultado
-            } else{
-                Write-Host "No se establecio la sesion en $computadora"
-            }        
+            $resultado = "Hora en $computer : $objectHora.Day/$objectHora.Month/$objectHora.Year $objectHora.Hour:$objectHora.Minute:$objectHora.Second"
+            Write-Host $resultado
+            Add-Content -Path $archivoSalida -Value $resultado       
         } catch {
-            $errorMensaje = "No se pudo obtener la hora en $computadora. Error: $_"
+            $errorMensaje = "Error al obtener obtener la hora en $computer. Error: $_"
             Write-Host $errorMensaje
             Add-Content -Path $archivoSalida -Value $errorMensaje
-
-            # Forzar la sincronización con el servidor de dominio
-            #Write-Host "Forzando sincronización de tiempo en $computadora con el servidor de dominio..."
-            #Invoke-Command -ComputerName $computadora -ScriptBlock { w32tm /resync /rediscover } -ErrorAction SilentlyContinue
         }
     } else {
-        $offlineMensaje = "La computadora $computadora no esta encendida o no responde al ping."
+        $offlineMensaje = "La computadora $computer no esta encendida o no responde al ping."
         Write-Host $offlineMensaje
         Add-Content -Path $archivoSalida -Value $offlineMensaje
     }
