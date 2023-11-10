d407de63b6cf27200836d8d16f5eca374e5ab0cc
oscarlim123 <oscarlim@protonmail.com>
2023-11-10 13:59:49 -0500

    Uso de Get-WmiObject

--- Get-TimeAD.ps1
+++ Get-TimeAD.ps1
@@ -1,34 +1,42 @@
 
-# Forzar la sincronización con el servidor de dominio
-#Write-Host "Forzando sincronización de tiempo en $computer con el servidor de dominio..."
-#Invoke-Command -ComputerName $computer -ScriptBlock { w32tm /resync /rediscover } -ErrorAction SilentlyContinue
+<# 
+.SYNOPSIS
+	Muestra la fecha y hora de las estaciones de un AD
+.PARAMETER
+.EXAMPLE
+	PS> ./Get-TimeAD.ps1
+.LINK
+
+.NOTES
+
+#>
 
 # Lista de nombres de computadoras del dominio
 $computerADList = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
 
 $archivoSalida = "HoraComputadorasAD.txt"
+$timexComputadoras = New-Object System.Collections.Generic.List[string]
 
 foreach ($computer in $computerADList) {
     $online = Test-Connection -ComputerName $computer -Count 2 -Quiet -ErrorAction SilentlyContinue
     
     if ($online) {
         try {
-            #$hora = Invoke-Command -ComputerName $computer -ScriptBlock { Get-Date } -ErrorAction Stop
             $objectHora = Get-WmiObject -Class Win32_LocalTime -ComputerName $computer -ErrorAction Stop
-
-            $resultado = "Hora en $computer : $objectHora.Day/$objectHora.Month/$objectHora.Year $objectHora.Hour:$objectHora.Minute:$objectHora.Second"
-            Write-Host $resultado
-            Add-Content -Path $archivoSalida -Value $resultado       
+            $resultado = "Hora en $computer : $($objectHora.Day)/$($objectHora.Month)/$($objectHora.Year) $($objectHora.Hour):$($objectHora.Minute):$($objectHora.Second)"
+            Write-Host $resultado 
+            $timexComputadoras.Add($resultado)     
         } catch {
             $errorMensaje = "Error al obtener obtener la hora en $computer. Error: $_"
             Write-Host $errorMensaje
-            Add-Content -Path $archivoSalida -Value $errorMensaje
+            $timexComputadoras.Add($errorMensaje)
         }
     } else {
         $offlineMensaje = "La computadora $computer no esta encendida o no responde al ping."
         Write-Host $offlineMensaje
-        Add-Content -Path $archivoSalida -Value $offlineMensaje
+        $timexComputadoras.Add($offlineMensaje)
     }
 }
 
+$timexComputadoras | Set-Content -Path $archivoSalida -Force
 Write-Host "Resultados guardados en $archivoSalida"
