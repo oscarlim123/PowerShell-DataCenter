3a9c6c26be2b63d37b2730ad4d8ead3cd18d24a5
oscarlim123 <oscarlim@protonmail.com>
2023-11-14 08:43:11 -0500

    Agregar parametro -AsJob para ejecucion en 2do plano

--- rsyncTimeAD.ps1
+++ rsyncTimeAD.ps1
@@ -1,6 +1,7 @@
 <# 
 .SYNOPSIS
-	Forzar la sincronización de las estaciones de un Directorio Activo con el servidor de dominio
+	Forzar la sincronización de las estaciones de un Directorio Activo con el servidor de dominio.
+    Lanza los comandos de resincronización de tiempo en segundo plano
 .EXAMPLE
 	PS> ./rsyncTimeAD.ps1
 .LINK
@@ -15,13 +16,13 @@ $timexComputadoras = New-Object System.Collections.Generic.List[string]
 
 foreach ($computer in $computerADList) {
     try {
-        $Session = New-PSSession -ComputerName $computer
+        $Session = New-PSSession -ComputerName $computer -ErrorAction Stop
 
         if ($Session.State -eq 'Opened') {           
             #region ParteModificable
                 $TimeZone = Invoke-Command -ComputerName $computer -ScriptBlock { 
                     w32tm /resync /rediscover 
-                } -ErrorAction SilentlyContinue
+                } -AsJob -ErrorAction SilentlyContinue
 
                 $rsync = "Sincronizacion de $computer"
                 Write-Host $rsync
@@ -37,7 +38,7 @@ foreach ($computer in $computerADList) {
         $rsync = "Error al establecer la sesión remota en $computer : $($_.exception.message)"
         $timexComputadoras.Add($rsync)
         Write-Host $rsync
-    }
+    } 
 }
 
 $timexComputadoras | Set-Content -Path $archivoSalida -Force
